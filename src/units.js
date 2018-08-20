dab35d3a69b423f8b0e9f00d0ef1469aa5f81a50
jardenliu <jardenliu1992@gmail.com>
2018-08-20 13:59:29 +0800

    fixed getFilter in windows

--- src/units.js
+++ src/units.js
@@ -2,7 +2,7 @@ const getFilter = (pagePath) => {
     let pagePaths = typeof pagePath === 'string' ? [pagePath] : pagePath
     let regs = []
     pagePaths.forEach(path => {
-        regs.push(path + '\/.*json$')  // eslint-disable-line
+        regs.push(path + '([\\/]|[\\\\]).*json$')  // eslint-disable-line
     })
     return new RegExp(regs.join('|'))
 }
